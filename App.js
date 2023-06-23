89cb4b3d78f1fbe7c098ea26cc96041dc01254a3
nitukumari <nituspj032001@gmail.com>
2023-06-23 22:40:52 +0530

    ignite --app

--- App.js
+++ App.js
@@ -1,36 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// nested div using html
-{/* <div id="parent">
-    <div id="child">
-        <h1>i am an html tag</h1>
-        <h2>i am h2 tag</h2>
-    </div>
-     <div id="child2">
-        <h1>i am an html tag</h1>
-        <h2>i am h2 tag</h2>
-    </div>
-</div> */}
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+        React.createElement("h1", {}, "This is Namaste React 🚀"),
+        React.createElement("h2", {}, "by nitu kumari"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I'm an h1 tag"),
+        React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+]);
 
-// ReactElement(object)=HTML(Browser understand)
-
-// nested div using react
-
-
-
-const parent = React.createElement('div',
-    { id: 'parent' },
-    [
-        React.createElement('div', { id: 'child' }, [
-            React.createElement('h1', {}, "i am h1 tag"),
-            React.createElement('h2', {}, "i am h2 tag")
-        ]),
-        React.createElement('div', { id: 'child2' }, [
-            React.createElement('h3', {}, "i am h3 tag"),
-            React.createElement('h4', {}, "i am h4 tag")
-        ])
-    ]);
-console.log(parent)
+console.log(parent); // object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(parent);
